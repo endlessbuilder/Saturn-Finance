6ae018c0d3c8c00bf912a9015057ce0052ff6d23
0xbear9999 <supernova1111a@outlook.com>
2024-05-16 14:22:35 +0200

    added: dependency and Account structure

--- programs/saturn-v1/src/lib.rs
+++ programs/saturn-v1/src/lib.rs
@@ -1,4 +1,15 @@
 use anchor_lang::prelude::*;
+use pyth_solana_receiver_sdk::price_update::{PriceUpdateV2};
+
+pub mod account;
+pub mod constants;
+pub mod error;
+// pub mod utils;
+
+use account::*;
+use constants::*;
+use error::*;
+// use utils::*;
 
 declare_id!("5mWSmPkAEesVq134hxA1gqFiwDXLArUacKXfmbmXwBBt");
 
@@ -9,7 +20,47 @@ pub mod saturn_v1 {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn ApplyBond(ctx: Context<ApplyBond>) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+#[instruction()]
+pub struct ApplyBond<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [TREASURY_SEED.as_ref()],
+        bump,
+    )]
+    pub treasury: Account<'info, Treasury>,
+
+    #[account(zero)]
+    pub escrow: AccountLoader<'info, Escrow>,
+
+    #[account(
+        mut,
+        constraint = creater_token_account.mint == *token_mint_address.to_account_info().key,
+        constraint = creater_token_account.owner == *admin.key,
+    )]
+    pub creater_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        constraint = dest_token_account.mint == *token_mint_address.to_account_info().key,
+        constraint = dest_token_account.owner == *treasury.to_account_info().key,
+    )]
+    pub dest_token_account: Account<'info, TokenAccount>,
+
+    pub price_update: Account<'info, PriceUpdateV2>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_mint_address: AccountInfo<'info>,
+}
