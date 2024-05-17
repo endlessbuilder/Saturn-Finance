19813ff895a6c16812323216448d263213fbcd95
0xbear9999 <supernova1111a@outlook.com>
2024-05-17 15:24:43 +0200

    added: collateral constants

--- programs/saturn-v1/src/constants.rs
+++ programs/saturn-v1/src/constants.rs
@@ -1,1 +1,4 @@
 pub const TREASURY_SEED: &str = "global-treasury";
+pub const USDC_MINT: &str = "9cmYPgxT1wGP6ySgSDHCmTrLYzeDp1iVssy4grjdjDyQ";
+pub const BONK_MINT: &str = "GAKS74QSGdt4tN4SLH6bHhJfAucYu3e8Dwf6hRRcJaU1";
+pub const SOL_MINT: &str = "11111111111111111111111111111111";
