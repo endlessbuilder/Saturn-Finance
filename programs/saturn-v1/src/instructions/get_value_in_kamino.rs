5d884ba11c9e8f37b1daff8131f83c3b1266ec46
toniandsol <toniandsol@gmail.com>
2024-06-25 20:10:59 -0400

    update: restruct getting value in kamino

--- programs/saturn-v1/src/instructions/get_value_in_kamino.rs
+++ programs/saturn-v1/src/instructions/get_value_in_kamino.rs
@@ -1,18 +1,39 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
+use serde::de::value;
 
 use crate::{error::*, kamino_utils};
 use crate::account::Treasury;
-use crate::constants::{TREASURY_AUTHORITY_SEED, TREASURY_METEORA_LP};
-use meteora::state::Vault;
+use crate::constants::*;
+use kamino_lending::state::{LendingMarket, Reserve, Obligation};
 
 
 #[derive(Accounts)]
 pub struct GetValueInKamino {
 
     pub lending_market: AccountLoader<'info, LendingMarket>,
-    #[account(mut, has_one = lending_market)]
-    pub withdraw_reserve: AccountLoader<'info, Reserve>,
+
+    #[account(
+        mut, 
+        has_one = lending_market,
+        constraint = sol_reserve.load()?.liquidity.mint_pubkey.to_string().as_str() == SOL_MINT,
+    )]
+    pub sol_reserve: AccountLoader<'info, Reserve>,
+
+    #[account(
+        mut, 
+        has_one = lending_market,
+        constraint = usdc_reserve.load()?.liquidity.mint_pubkey.to_string().as_str() == USDC_MINT,
+    )]
+    pub usdc_reserve: AccountLoader<'info, Reserve>,
+
+    #[account(
+        mut, 
+        has_one = lending_market,
+        constraint = bonk_reserve.load()?.liquidity.mint_pubkey.to_string().as_str() == BONK_MINT,
+    )]
+    pub bonk_reserve: AccountLoader<'info, Reserve>,
+
     #[account(mut,
         has_one = lending_market,
         has_one = owner
@@ -21,24 +42,61 @@ pub struct GetValueInKamino {
 
 }
 
-pub fn handle(ctx: Context<GetValueInKamino>) -> Result<()> {
+pub fn handle(ctx: Context<GetValueInKamino>) -> Result<([u64; 3])> {
     // # get kamino value
     let clock = Clock::get();
-
-    let obligation_amount = kamino_utils::calcu_obligation_collateral(
+    let sol_reserve = &mut ctx.accounts.sol_reserve.load_mut().unwrap();
+    let obligation_amount1 = kamino_utils::calcu_obligation_collateral(
         lending_market,
-        reserve,
+        sol_reserve,
         obligation,
-        collateral_amount,
+        collateral_amount1,
         clock.slot,
-        ctx.accounts.withdraw_reserve.key(),
+        ctx.accounts.sol_reserve.key(),
     )?;
-    let liquidity_amount = kamino_utils::redeem_reserve_collateral(
-        reserve,
-        obligation_amount,
+    let liquidity_amount1 = kamino_utils::redeem_reserve_collateral(
+        sol_reserve,
+        obligation_amount1,
         clock,
         true,
     )?;
 
-   Ok(())
+    let usdc_reserve = &mut ctx.accounts.usdc_reserve.load_mut().unwrap();
+    let obligation_amount2 = kamino_utils::calcu_obligation_collateral(
+        lending_market,
+        usdc_reserve,
+        obligation,
+        collateral_amount2,
+        clock.slot,
+        ctx.accounts.usdc_reserve.key(),
+    )?;
+    let liquidity_amount2 = kamino_utils::redeem_reserve_collateral(
+        usdc_reserve,
+        obligation_amount2,
+        clock,
+        true,
+    )?;
+
+    let bonk_reserve = &mut ctx.accounts.bonk_reserve.load_mut().unwrap();
+    let obligation_amount3 = kamino_utils::calcu_obligation_collateral(
+        lending_market,
+        bonk_reserve,
+        obligation,
+        collateral_amount3,
+        clock.slot,
+        ctx.accounts.bonk_reserve.key(),
+    )?;
+    let liquidity_amount3 = kamino_utils::redeem_reserve_collateral(
+        bonk_reserve,
+        obligation_amount3,
+        clock,
+        true,
+    )?;
+
+    let values: [u64; 3] = [0, 0, 0];
+    values[0] = liquidity_amount1.into();
+    values[1] = liquidity_amount2.into();
+    values[2] = liquidity_amount3.into();
+
+   Ok((values))
 }
